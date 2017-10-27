0a871496391cc7a44240060127643e2b8fe715b2
Natrada Chairat <natrada_chairat@cmu.ac.th>
2017-10-27 15:37:52 +0700

    final 5.10

---
+++ src/main/java/camt/cbsd/lab05/entity/Course.java
@@ -0,0 +1,24 @@
+package camt.cbsd.lab05.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@JsonIgnoreProperties (ignoreUnknown = true)
+@Builder
+public class Course {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    long id;
+    String courseId;
+    String courseName;
+}
